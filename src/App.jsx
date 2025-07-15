aebc4fcb1b1d466847f1f08883c36d231626b6a4
Jules-Elvin Andrade <juleselvandrade@gmail.com>
2025-07-15 13:02:42 -0700

    most main features added: side info panel, main panel can list fetched api data

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+import CardInfo from './components/CardInfo'
 import './App.css'
+const API_KEY = import.meta.env.VITE_SPOONACULAR_KEY
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [recipeList, setRecipeList] = useState(null);
+
+  useEffect(() => {
+    const fetchRecipeList = async () => {
+      let query = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&number=10`;
+      const response = await fetch(query);
+      const json = await response.json();
+
+      console.log(json);
+      setRecipeList(json);
+    }
+
+    fetchRecipeList().catch(console.error);
+  }, []) // this will probably change i just wanted to render it on first load
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+    <div className="main-container">
+
+      <div className="nav-panel">
+        <h2> Food Forager! 📖 </h2>
+        <p> 
+          Search, filter and learn about your favorite foods or foods you've never heard of before!
         </p>
+        <p className="add-info"> 
+          CREDITS: spoonacular API 
+          </p>
+        <p className="add-info"> 
+          DV (daily value) refers to the recommended daily intake based on the average human, will vary depending on your body ** 
+          </p>
+        </div>
+
+      <div className="main-panel">
+        <div className="summary-cntr">
+          bazinga
+        </div>
+        <div className="display-cntr">
+          {recipeList && recipeList.results.map((result) => (
+            <CardInfo
+              key={result.id}
+              image={result.image}
+              title={result.title}
+              id={result.id}
+            />
+          ))}
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+    </div>
   )
 }
+
 
 export default App
---
+++ src/components/CardInfo.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react"
+import "../CardInfo.css"
+const API_KEY = import.meta.env.VITE_SPOONACULAR_KEY
+
+const CardInfo = ( {image, id, title} ) => {
+    const [foodInfo, setFoodInfo] = useState(null);
+    const [calories, setCalories] = useState(0);
+    const [proteinDV, setProteinDV] = useState(0);
+    const [carbsDV, setCarbsDV] = useState(0);
+    const [fatDV, setFatDV] = useState(0);
+
+    useEffect(() => {
+        const fetchNutrition = async () => {
+            let query = `https://api.spoonacular.com/recipes/${id}/nutritionWidget.json?apiKey=${API_KEY}`;
+            const response = await fetch(query);
+            const json = await response.json();
+
+            setCalories(json.calories);
+            setProteinDV(json.good[0].percentOfDailyNeeds);
+            setCarbsDV(json.bad[3].percentOfDailyNeeds);
+            setFatDV(json.bad[1].percentOfDailyNeeds);
+        }
+
+        fetchNutrition().catch(console.error);
+    }, [id])
+
+    return (
+        <div className="card-cntr">
+            <img src={image} width={200} height={150}></img>
+            <h4> {title} </h4>
+            <p> Calories: {calories} </p>
+            <p> <span className="percentages"> {proteinDV}% </span> DV of protein!</p>
+            <p> <span className="percentages"> {carbsDV}% </span> DV of carbs! </p>
+            <p> <span className="percentages"> {fatDV}% </span> DV of fat! </p>
+        </div>
+    )
+}
+export default CardInfo;
